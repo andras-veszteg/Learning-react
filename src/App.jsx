ced6ce4fbac6183a96a9cfb4fbd92d7d0dc74f03
Veszteg András <veszteg.andras@gmail.com>
2026-08-07 22:45:49 +0200

    "Integrate dynamic content rendering for selected tab using EXAMPLES data"

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import {CORE_CONCEPTS} from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton.jsx";
+import {EXAMPLES} from "./data";
 
 
 function App() {
-    const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+    const [selectedTopic, setSelectedTopic] = useState('components');
     function handleSelect(selectedButton){
         setSelectedTopic(selectedButton);
         console.log(selectedTopic);
@@ -37,7 +38,15 @@ function App() {
                   <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
                   <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
               </menu>
-              {selectedTopic}
+              <div id="tab-content">
+                  <h3>{EXAMPLES[selectedTopic].title}</h3>
+                  <p>{EXAMPLES[selectedTopic].description}</p>
+                  <pre>
+                      <code>
+                          {EXAMPLES[selectedTopic].code}
+                      </code>
+                  </pre>
+              </div>
           </section>
       </main>
     </div>
