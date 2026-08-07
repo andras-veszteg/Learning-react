854adddad953876d72bdef59aba775094963e80c
Veszteg András <veszteg.andras@gmail.com>
2026-08-07 22:36:29 +0200

    "Add examples data and implement state management for tab selection in App component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 import {CORE_CONCEPTS} from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept";
@@ -5,8 +7,10 @@ import TabButton from "./components/TabButton.jsx";
 
 
 function App() {
+    const [selectedTopic, setSelectedTopic] = useState('Please click a button');
     function handleSelect(selectedButton){
-        console.log(selectedButton);
+        setSelectedTopic(selectedButton);
+        console.log(selectedTopic);
     }
   return (
     <div>
@@ -33,7 +37,7 @@ function App() {
                   <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
                   <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
               </menu>
-              
+              {selectedTopic}
           </section>
       </main>
     </div>
